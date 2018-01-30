649def5880676f2c428948460ea66f0325df939a
raddude <jjmarchewitz@gmail.com>
2018-01-29 21:21:29 -0500

    Mid-work push, first theoretically working version of the DriveTrain and Climb classes

--- 2018_Robot/src/org/usfirst/frc/team1787/robot/Robot.java
+++ 2018_Robot/src/org/usfirst/frc/team1787/robot/Robot.java
@@ -1,21 +1,36 @@
 package org.usfirst.frc.team1787.robot;
 
+import org.usfirst.frc.team1787.subsystems.Autonomous;
+import org.usfirst.frc.team1787.subsystems.DriveTrain;
+
+
 /* CLASS DEFINITION:
  * This class is the main class for the robot, which ties all parts together and is automatically called upon by the RoboRIO
  */
 
-
-
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends IterativeRobot {
 	
-	TalonSRX testTalon;
+	protected int farfar38;
+	
+	private DriveTrain driveTrain = DriveTrain.getInstance();
+	
+	private final int RIGHT_JOYSTICK_ID = 0;
+	private final int LEFT_JOYSTICK_ID = 1;
+	private Joystick rightStick = new Joystick(RIGHT_JOYSTICK_ID);
+	private Joystick leftStick = new Joystick(LEFT_JOYSTICK_ID);
+	private int JOYSTICK_ROTATION_AXIS = 2;
+	private int JOYSTICK_SLIDER_AXIS = 3;
+	
+	//Input options
+	private int SHOOT_CUBES_BUTTON;
 
 	@Override
 	public void robotInit() {
@@ -34,7 +49,7 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		
+		driveTrain.arcadeDrive(-rightStick.getY(), rightStick.getX());
 	}
 
 	@Override
--- 2018_Robot/src/org/usfirst/frc/team1787/subsystems/Climb.java
+++ 2018_Robot/src/org/usfirst/frc/team1787/subsystems/Climb.java
@@ -1,9 +1,45 @@
 package org.usfirst.frc.team1787.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /* CLASS DEFINITION:
  * This class handles everything related to climbing, including the 3ft pneumatic cylinder and the backup alternative method of climbing (likely a winch)
  */
 
 public class Climb {
-
+	
+	private final int CLIMB_EXTEND_SOLENOID_ID = 0;
+	private final int CLIMB_RETRACT_SOLENOID_ID = 1;
+	
+	private DoubleSolenoid climbingSolenoid = new DoubleSolenoid(CLIMB_EXTEND_SOLENOID_ID, CLIMB_RETRACT_SOLENOID_ID);
+	
+	public final DoubleSolenoid.Value EXTEND = DoubleSolenoid.Value.kForward;
+	public final DoubleSolenoid.Value RETRACT = DoubleSolenoid.Value.kReverse;
+	
+	private static final Climb instance = new Climb();
+	
+	
+	private Climb() {
+		
+	}
+	
+	
+	/**
+	 * 
+	 * Takes 1 parameter - Can either be EXTEND or RETRACT
+	 */
+	public void movePiston(DoubleSolenoid.Value desiredState) {
+		climbingSolenoid.set(desiredState);
+	}
+	
+	
+	public void pushDataToShuffleboard() {
+		SmartDashboard.putBoolean("Climb Extended: ", climbingSolenoid.get() == this.EXTEND);
+	}
+	
+	
+	public static Climb getInstance() {
+	    return instance;
+	  }
 }
