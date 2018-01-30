5cf7156b91c7287b6ce1ea7d11d0b627ea3690fa
raddude <raddude@users.noreply.github.com>
2018-01-30 08:45:51 -0500

    It is the morning that I spoke of yesterday, and I just migrated all of my code to my own repo and Robot.java is now empty.

--- 2018_Robot/src/org/usfirst/frc/team1787/robot/Robot.java
+++ 2018_Robot/src/org/usfirst/frc/team1787/robot/Robot.java
@@ -1,35 +1,15 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package org.usfirst.frc.team1787.robot;
 
-/* CLASS DEFINITION:
- * This class is the main class for the robot, which ties all parts together and is automatically called upon by the RoboRIO
- */
+import edu.wpi.first.wpilibj.TimedRobot;
 
-import org.usfirst.frc.team1787.subsystems.Autonomous;
-import org.usfirst.frc.team1787.subsystems.Climb;
-import org.usfirst.frc.team1787.subsystems.DriveTrain;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-
-public class Robot extends IterativeRobot {
-	
-	protected int farfar38;
-	
-	private DriveTrain driveTrain = DriveTrain.getInstance();
-	private Climb climb = Climb.getInstance();
-	
-	private final int RIGHT_JOYSTICK_ID = 0;
-	private final int LEFT_JOYSTICK_ID = 1;
-	private Joystick rightStick = new Joystick(RIGHT_JOYSTICK_ID);
-	private Joystick leftStick = new Joystick(LEFT_JOYSTICK_ID);
-	private int JOYSTICK_ROTATION_AXIS = 2;
-	private int JOYSTICK_SLIDER_AXIS = 3;
-	
-	//Input options
-	private int SHOOT_CUBES_BUTTON;
+public class Robot extends TimedRobot {
 
 	@Override
 	public void robotInit() {
@@ -38,23 +18,17 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void autonomousInit() {
-
+		
 	}
-
+	
 	@Override
 	public void autonomousPeriodic() {
-
+		
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		driveTrain.arcadeDrive(-rightStick.getY(), rightStick.getX());
 		
-		
-		
-		
-		driveTrain.pushDataToShuffleboard();
-		climb.pushDataToShuffleboard();
 	}
 
 	@Override
--- 2018_Robot/src/org/usfirst/frc/team1787/subsystems/Autonomous.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team1787.subsystems;
-
-public class Autonomous {
-	//oh boy
-}
